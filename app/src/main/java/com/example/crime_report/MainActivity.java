4881da88758ab802c9c91e940d9a508d6c34feb0
mangorepublic <hopefuledi@gmail.com>
2022-01-18 16:22:43 +0000

    another commit

--- app/src/main/java/com/example/crime_report/MainActivity.java
+++ app/src/main/java/com/example/crime_report/MainActivity.java
@@ -1,14 +1,110 @@
 package com.example.crime_report;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView profile;
+    private LottieAnimationView btnlocation;
+    private Boolean record = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private double lat,lon;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profile = (ImageView) findViewById(R.id.profile);
+        btnlocation = (LottieAnimationView) findViewById(R.id.location);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+//        locationRequest = com.google.android.gms.location.LocationRequest.create();
+//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        locationRequest.setInterval(20);
+//
+//        locationCallback = new LocationCallback() {
+//            @Override
+//            public void onLocationResult(@NonNull LocationResult locationResult) {
+//
+//                for (Location location : locationResult.getLocations()){
+//                    if (location != null){
+//                               lat = location.getLatitude();
+//                               lon = location.getLongitude();
+//                    }
+//                }
+//            }
+//        };
+
+
+
+        //setting onclick listener for location
+        btnlocation.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null){
+                            Double lat = location.getLatitude();
+                            Double lon = location.getLongitude();
+                            Snackbar.make(view,""+lat+" "+lon,Snackbar.LENGTH_LONG).show();
+                        }else{
+                            Snackbar.make(view,"Problem getting geolocation",Snackbar.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+//                Snackbar.make(view,""+lat+" "+lon,Snackbar.LENGTH_SHORT).show();
+//                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+
+
+
+            }
+        });
+
+
+
+        //setting onclick listener for profile
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to profile activity
+                Intent intent = new Intent(MainActivity.this, com.example.crime_report.profile.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter_from_right,R.anim.exit_through_right);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //do nothing
     }
 }
--- app/src/main/java/com/example/crime_report/profile.java
+++ app/src/main/java/com/example/crime_report/profile.java
@@ -1,14 +1,95 @@
 package com.example.crime_report;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile extends AppCompatActivity {
+    private TextView closeprofile,fullname,myemail;
+    private Button logout;
+    private FirebaseAuth mAuth;
+    private UserClass userClass;
+    private String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        closeprofile = (TextView) findViewById(R.id.clprofile);
+        logout   = (Button) findViewById(R.id.logout);
+        fullname = (TextView) findViewById(R.id.fullname);
+        myemail = (TextView) findViewById(R.id.myemail);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!=null){
+            email = user.getEmail();
+        }
+
+        //getting user information from the database
+        Query query = FirebaseDatabase.getInstance().getReference("user").orderByChild("email").equalTo(email);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    userClass = ds.getValue(UserClass.class);
+                }
+
+                //showing information on textview
+                if (userClass != null){
+                    fullname.setText(String.format("%s %s", userClass.getFname(), userClass.getLname()));
+                    myemail.setText(userClass.getEmail());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //setting onclick listener for close profile
+        closeprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go back to dashboard
+                Intent intent = new Intent(profile.this,MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter_from_left,R.anim.exit_through_left);
+                finish();
+            }
+        });
+
+        //setting onclick listener for logout button
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(profile.this,signin_signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //do nothing
     }
 }
